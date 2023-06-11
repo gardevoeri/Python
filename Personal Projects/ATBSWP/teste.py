e20062d0061b1cf018c35d5d70443964b04dad7d
gardevoeri <je.santos.silva@gmail.com>
2023-06-11 17:20:31 -0300

    book exercises

---
+++ Personal Projects/ATBSWP/teste.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+print(
+    f"Olá, {nome}! Ano que vem você terá {idade + 1} anos e seu nome tem {len(nome)} letras."
+)
