e3941c107200d2ca076430454953dadeb3264703
gardevoeri <je.santos.silva@gmail.com>
2023-09-28 14:30:17 -0300

    mysql config

---
+++ Personal Projects/Database/mysql-test.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host="localhost", password="Je@112358", user="root", database="learning"
+)
+
+db = connection.cursor()
+
+
+# if connection.is_connected():
+#    print("Connection estabilished...")
